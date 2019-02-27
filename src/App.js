a81e02a8aeca8133e4d7c46377ad9d1aeaef686c
Jaini Guevara <jaini.guevara@gmail.com>
2019-02-27 20:52:30 +0800

    Setup i18next resource with namespace

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {t('Welcome to React')} <br/>
+          {t('welcome')} <br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,33 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+// import Backend from 'i18next-xhr-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 // the translations
 // (tip move them in a JSON file and import them)
 const resources = {
   en: {
-    translation: {
-      'Welcome to React': 'Welcome to React and react-i18next'
+    general: {
+      'welcome': 'Welcome to React and react-i18next'
     }
   }
 }
 
+const defaultOptions = {
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  ns: ['general'],
+  defaultNS: 'general',
+  fallbackNS: 'general',
+  nsSeparator: '::',
+  keySeparator: '::',
+}
+
 i18n
+  // .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    lng: 'en',
-
-    keySeparator: false, // we do not use keys in form messages.welcome
-
-    interpolation: {
-      escapeValue: false // react already safes from xss
-    }
-  })
+  .init(defaultOptions)
 
 export default i18n
