b2cd63b7a1788115d447c77cd02800ea9a24e37f
Jaini Guevara <jaini.guevara@gmail.com>
2019-05-21 20:02:49 +0800

    Added static data

--- src/components/Skills/data.js
+++ src/components/Skills/data.js
@@ -3,6 +3,7 @@ import red from '@material-ui/core/colors/red'
 
 import JSLogo from './../../images/js-logo.svg'
 import ReactLogo from './../../images/react-logo.svg'
+import ReactNativeLogo from './../../images/react-native-logo.svg'
 import ReduxLogo from './../../images/redux-logo.svg'
 import MaterialUILogo from './../../images/material-ui-logo-white.svg'
 import ChartJSLogo from './../../images/chartjs-logo.svg'
@@ -17,35 +18,42 @@ export const frontEndObjects = () => {
       backgroundColor: '#F7DF1E',
       hoverBackgroundColor: '#F3E272',
       logo: JSLogo,
-      data: [9, undefined, undefined, undefined, undefined]
+      data: [9, undefined, undefined, undefined, undefined, undefined]
     },
     {
       label: 'React.js',
       backgroundColor: '#61DAFB',
       hoverBackgroundColor: '#80E2FB',
       logo: ReactLogo,
-      data: [undefined, 9, undefined, undefined, undefined,]
+      data: [undefined, 9, undefined, undefined, undefined, undefined]
+    },
+    {
+      label: 'React Native',
+      backgroundColor: '#FFFF',
+      hoverBackgroundColor: '#EEE',
+      logo: ReactNativeLogo,
+      data: [undefined, undefined, 6, undefined, undefined, undefined]
     },
     {
       label: 'Redux.js',
       backgroundColor: '#764ABC',
       hoverBackgroundColor: '#916EC9',
       logo: ReduxLogo,
-      data: [undefined, undefined, 8, undefined, undefined, ]
+      data: [undefined, undefined, undefined, 8, undefined, undefined]
     },
     {
       label: 'Material-UI',
       backgroundColor: blue[600],
       hoverBackgroundColor: blue[500],
       logo: MaterialUILogo,
-      data: [undefined, undefined, undefined, 8, undefined]
+      data: [undefined, undefined, undefined, undefined, 8, undefined]
     },
     {
       label: 'Chart.js',
       backgroundColor: red[400],
       hoverBackgroundColor: red[300],
       logo: ChartJSLogo,
-      data: [undefined, undefined, undefined, undefined, 7]
+      data: [undefined, undefined, undefined, undefined, undefined, 7]
     },
   ]
   return {
